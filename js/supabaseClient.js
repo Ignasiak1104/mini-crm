21897ebf814380a8fc3e81539f445d20db277303
Ignasiak1104 <jedrzejignasiak@gmail.com>
2025-05-23 14:51:10 +0200

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,1 +1,8 @@
+// js/supabaseClient.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
+// Upewnij się, że te dane są poprawne dla Twojego projektu Supabase
+const supabaseUrl = 'https://acwseeemqkmwxncektfz.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjd3NlZWVtcWttd3huY2VrdGZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MTY2MDEsImV4cCI6MjA2MzQ5MjYwMX0.y8pPbzsgIkpEl6CHNYpBS2lRdx5DB6A7DupAcyjksvs';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
