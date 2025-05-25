4d4a7b55c9c01e53fbd8f8ab9b3a0c8977ca19c0
Ignasiak1104 <jedrzejignasiak@gmail.com>
2025-05-25 14:06:35 +0200

    Update domElements.js

--- js/domElements.js
+++ js/domElements.js
@@ -1,21 +1,18 @@
 // js/domElements.js
 export const DOM = {
-    // Kontener na alerty
-    alertContainer: document.getElementById('alertContainer'),
+    alertContainer: document.getElementById('alertContainer'), // Musi być w HTML
 
-    // Modale formularzy
+    // Modale - te są potrzebne do otwierania/zamykania
     contactFormModal: document.getElementById('contactFormModal'),
     companyFormModal: document.getElementById('companyFormModal'),
     quickAddCompanyModal: document.getElementById('quickAddCompanyModal'),
     transactionFormModal: document.getElementById('transactionFormModal'),
     deleteConfirmModal: document.getElementById('deleteConfirmModal'),
-
-    // Modale szczegółów
     contactDetailsModal: document.getElementById('contactDetailsModal'),
     companyDetailsModal: document.getElementById('companyDetailsModal'),
     transactionDetailsModal: document.getElementById('transactionDetailsModal'),
 
-    // Przyciski zamykania modali (X)
+    // Przyciski zamykania modali (X) i inne przyciski w modalach
     closeContactFormModalButton: document.getElementById('closeContactFormModalButton'),
     closeCompanyFormModalButton: document.getElementById('closeCompanyFormModalButton'),
     closeQuickAddCompanyModalButton: document.getElementById('closeQuickAddCompanyModalButton'),
@@ -24,11 +21,9 @@ export const DOM = {
     closeContactDetailsModalButton: document.getElementById('closeContactDetailsModalButton'),
     closeCompanyDetailsModalButton: document.getElementById('closeCompanyDetailsModalButton'),
     closeTransactionDetailsModalButton: document.getElementById('closeTransactionDetailsModalButton'),
-
-    // Przyciski akcji w modalach (Anuluj, Zamknij itp.)
     cancelQuickAddCompanyButton: document.getElementById('cancelQuickAddCompanyButton'),
-    cancelDeleteButton: document.getElementById('cancelDeleteButton'), // Przycisk "Anuluj" w deleteConfirmModal
-    confirmDeleteButton: document.getElementById('confirmDeleteButton'), // Przycisk "Usuń" w deleteConfirmModal
+    cancelDeleteButton: document.getElementById('cancelDeleteButton'),
+    confirmDeleteButton: document.getElementById('confirmDeleteButton'), // Musi być w HTML
     closeContactDetailsViewButton: document.getElementById('closeContactDetailsViewButton'),
     closeCompanyDetailsViewButton: document.getElementById('closeCompanyDetailsViewButton'),
     closeTransactionDetailsViewButton: document.getElementById('closeTransactionDetailsViewButton'),
@@ -41,27 +36,15 @@ export const DOM = {
     // Elementy sekcji Transakcji (widoki, przełączniki)
     transactionListView: document.getElementById('transactionListView'),
     transactionKanbanView: document.getElementById('transactionKanbanView'),
-    toggleTransactionViewList: document.getElementById('toggleTransactionViewList'),
-    toggleTransactionViewKanban: document.getElementById('toggleTransactionViewKanban'),
+    toggleTransactionViewList: document.getElementById('toggleTransactionViewList'), // Musi być w HTML
+    toggleTransactionViewKanban: document.getElementById('toggleTransactionViewKanban'), // Musi być w HTML
 
-    // Formularze (do podpięcia event listenerów submit)
-    contactForm: document.getElementById('contactForm'),
-    companyForm: document.getElementById('companyForm'),
-    quickAddCompanyForm: document.getElementById('quickAddCompanyForm'),
-    transactionForm: document.getElementById('transactionForm'),
-    taskForm: document.getElementById('taskForm'),
-    salesProcessForm: document.getElementById('salesProcessForm'),
-    transactionStageForm: document.getElementById('transactionStageForm'),
-    customContactFieldForm: document.getElementById('customContactFieldForm'),
-    customCompanyFieldForm: document.getElementById('customCompanyFieldForm'),
+    // Komunikat w modalu usuwania
+    deleteMessage: document.getElementById('deleteMessage'),
 
-
-    // Inne potrzebne elementy (uzupełnij w miarę potrzeb)
-    deleteMessage: document.getElementById('deleteMessage'), // Wiadomość w deleteConfirmModal
-
-    // Możesz tu dodać więcej elementów, jeśli są często używane
-    // np. przyciski otwierania modali z formularzami
+    // Przyciski otwierania głównych modali formularzy
     openContactFormModalButton: document.getElementById('openContactFormModalButton'),
     openCompanyFormModalButton: document.getElementById('openCompanyFormModalButton'),
     openTransactionFormModalButton: document.getElementById('openTransactionFormModalButton'),
+    //addTaskButton: document.getElementById('addTaskButton'), // Dla taskForm, jeśli nie jest częścią samego formularza
 };
